5555ec27b9219cfb6d3b09ccb601286965657b93
bbb <bistabistan@gmail.com>
2025-08-25 11:51:41 +0545

    some code changes

---
+++ image resizer/main.py
@@ -0,0 +1,16 @@
+import cv2
+
+src = cv2.imread("image resizer/traveller.jpg")
+
+scale_percent = 50
+
+width = int(src.shape[1] * scale_percent /100 )
+height = int(src.shape[0] * scale_percent /100 )
+
+
+dsize = (width, height)
+
+
+output = cv2.resize(src, dsize)
+
+cv2.imwrite("image resizer/resized.jpg", output)
